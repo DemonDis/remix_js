8a317627391a0c858e2cc8812cd9296866cf72ad
DemonDis <50297797+DemonDis@users.noreply.github.com>
2023-01-18 02:34:10 +0000

    :construction: postcss

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,19 @@
+import { Links } from "@remix-run/react";
+
 import styles from "~/styles/login.css";
+import stylesapp from "~/styles/app.css";
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [
+    { rel: "stylesheet", href: styles },
+    { rel: "stylesheet", href: stylesapp },
+  ];
 }
 
 export default function Index() {
   return (
     <div className="wrapper --bs-secondary-color">
+      <h2 className="postcss-text">POSTCSS</h2>
       <h1 className="text-6xl font-bold text-red-700 ">Welcome to Remix</h1>
     </div>
   );
